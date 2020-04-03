505f9171e783e8b328fb1f815ba5aad9e7d15458
Kevin Mathias <komathias1@gmail.com>
2020-04-03 22:47:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,17 @@
-# code here!
+
+
+class School
+
+attr_accessor :name, :roster
+
+def initialize(name)
+  @name = name
+  @roster = {}
+end
+
+def add_student(student, grade)
+  @roster ||= []
+  @roster << student
+end
+
+end
