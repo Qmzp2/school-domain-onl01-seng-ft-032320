97ef86a246de2e70db659994eb76c468f0bfa4dd
Kevin Mathias <komathias1@gmail.com>
2020-04-03 22:50:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -4,14 +4,16 @@ class School
 
 attr_accessor :name, :roster
 
+ROSTER = {}
+
 def initialize(name)
   @name = name
   @roster = {}
 end
 
-def add_student(student, grade)
-  @roster ||= []
-  @roster << student
+def add_student=(student, grade)
+  ROSTER << {student, grade}
+
 end
 
 end
